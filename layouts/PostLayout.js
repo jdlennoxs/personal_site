623447dc53ba13ae4748bf715f80d2b5e6744fa6
Jack Scott <jack.scott10@gmail.com>
2021-06-26 15:09:00 +0100

    Alter posts layout

--- layouts/PostLayout.js
+++ layouts/PostLayout.js
@@ -24,16 +24,6 @@ export default function PostLayout({ children, frontMatter, next, prev }) {
         <div className="xl:divide-y xl:divide-gray-200 xl:dark:divide-gray-700">
           <header className="pt-6 xl:pb-6">
             <div className="space-y-1 text-center">
-              <dl className="space-y-10">
-                <div>
-                  <dt className="sr-only">Published on</dt>
-                  <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                    <time dateTime={date}>
-                      {new Date(date).toLocaleDateString(siteMetadata.locale, postDateTemplate)}
-                    </time>
-                  </dd>
-                </div>
-              </dl>
               <div>
                 <PageTitle>{title}</PageTitle>
               </div>
@@ -43,7 +33,7 @@ export default function PostLayout({ children, frontMatter, next, prev }) {
             className="pb-8 divide-y divide-gray-200 xl:divide-y-0 dark:divide-gray-700 xl:grid xl:grid-cols-4 xl:gap-x-6"
             style={{ gridTemplateRows: 'auto 1fr' }}
           >
-            <dl className="pt-6 pb-10 xl:pt-11 xl:border-b xl:border-gray-200 xl:dark:border-gray-700">
+            <dl className="xl:hidden pt-6 pb-10">
               <dt className="sr-only">Authors</dt>
               <dd>
                 <ul className="flex justify-center space-x-8 xl:block sm:space-x-12 xl:space-x-0 xl:space-y-8">
@@ -87,7 +77,48 @@ export default function PostLayout({ children, frontMatter, next, prev }) {
               </div>
             </div>
             <footer>
+              <dl className="hidden xl:block pt-6 pb-10 xl:pt-11 xl:border-b xl:border-gray-200 xl:dark:border-gray-700">
+                <dt className="sr-only">Authors</dt>
+                <dd>
+                  <ul className="flex justify-center space-x-8 xl:block sm:space-x-12 xl:space-x-0 xl:space-y-8">
+                    <li className="flex items-center space-x-2">
+                      <Image
+                        src={siteMetadata.image}
+                        width="38px"
+                        height="38px"
+                        alt="avatar"
+                        className="w-10 h-10 rounded-full"
+                      />
+                      <dl className="text-sm font-medium leading-5 whitespace-nowrap">
+                        <dt className="sr-only">Name</dt>
+                        <dd className="text-gray-900 dark:text-gray-100">{siteMetadata.author}</dd>
+                        {typeof siteMetadata.twitter === 'string' && (
+                          <>
+                            <dt className="sr-only">Twitter</dt>
+                            <dd>
+                              <Link
+                                href={siteMetadata.twitter}
+                                className="text-blue-500 hover:text-blue-600 dark:hover:text-blue-400"
+                              >
+                                {siteMetadata.twitter.replace('https://twitter.com/', '@')}
+                              </Link>
+                            </dd>
+                          </>
+                        )}
+                      </dl>
+                    </li>
+                  </ul>
+                </dd>
+              </dl>
               <div className="text-sm font-medium leading-5 divide-gray-200 xl:divide-y dark:divide-gray-700 xl:col-start-1 xl:row-start-2">
+                <div className="py-4 xl:py-8">
+                  <h2 className="text-xs tracking-wide text-gray-500 uppercase dark:text-gray-400">
+                    Published
+                  </h2>
+                  <time dateTime={date}>
+                    {new Date(date).toLocaleDateString(siteMetadata.locale, postDateTemplate)}
+                  </time>
+                </div>
                 {tags && (
                   <div className="py-4 xl:py-8">
                     <h2 className="text-xs tracking-wide text-gray-500 uppercase dark:text-gray-400">
