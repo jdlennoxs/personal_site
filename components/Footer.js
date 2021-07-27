d9b944698d9b6ebed47b89aa95d7440b11c2fa7f
Jack Scott <jack.scott10@gmail.com>
2021-07-27 23:38:32 +0100

    Use shared social icons

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 import tw, { styled } from 'twin.macro'
 import Link from './Link'
 import siteMetadata from '@/data/siteMetadata'
-import SocialIcon from '@/components/social-icons'
+import SocialIcons from '@/components/SocialIcons'
 
 const FooterContainer = styled.footer(() => [tw`bg-gray-50 w-full border-t border-gray-200`])
 const FooterContent = styled.div(() => [tw`flex flex-col items-center mt-10`])
@@ -10,14 +10,7 @@ export default function Footer() {
   return (
     <FooterContainer>
       <FooterContent>
-        <div tw="flex mb-3 space-x-4">
-          {/* <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size="6" /> */}
-          <SocialIcon kind="github" href={siteMetadata.github} size="6" />
-          {/* <SocialIcon kind="facebook" href={siteMetadata.facebook} size="6" /> */}
-          {/* <SocialIcon kind="youtube" href={siteMetadata.youtube} size="6" /> */}
-          <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size="6" />
-          {/* <SocialIcon kind="twitter" href={siteMetadata.twitter} size="6" /> */}
-        </div>
+        <SocialIcons />
         <div className="flex mb-2 space-x-2 text-xs text-gray-500 dark:text-gray-400">
           <div>{siteMetadata.author}</div>
           <div>{` • `}</div>
